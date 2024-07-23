cb3d7b24bf6b350575fad1f685dfe91e74f81dfc
mmulyana <mulyan.t20@gmail.com>
2024-07-24 00:20:50 +0700

    feat: new routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const auth = require('./routes/auth')
 const kriteria = require('./routes/kriteria')
 const pegawai = require('./routes/pegawai')
+const akun = require('./routes/akun')
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(cors())
 let api = express.Router()
 
 api.use(auth)
+api.use(akun)
 api.use(pegawai)
 api.use(kriteria)
 
