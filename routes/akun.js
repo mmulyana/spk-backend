9f56071ddb386436faa0946b94a0c0895980d4b1
mmulyana <mulyan.t20@gmail.com>
2024-07-24 00:19:52 +0700

    feat(akun): routes

--- routes/akun.js
+++ routes/akun.js
@@ -1,11 +1,18 @@
 const { Router } = require('express')
+const {
+  createAkunHandler,
+  updateAkunHandler,
+  deleteAkunHandler,
+  getAkunHandler,
+  getAllAkunHandler,
+} = require('../controller/akun.controller')
 
 const router = Router()
 
-router.post('/akun')
-router.patch('/akun/:id')
-router.delete('/akun/:id')
-router.get('/akun/:id')
-router.get('/akun')
+router.post('/akun', createAkunHandler)
+router.patch('/akun/:id', updateAkunHandler)
+router.delete('/akun/:id', deleteAkunHandler)
+router.get('/akun/:id', getAkunHandler)
+router.get('/akun', getAllAkunHandler)
 
 module.exports = router
