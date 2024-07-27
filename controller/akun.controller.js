744838d0f5f4d5aae70b66a700fd840dde8a0ad4
mmulyana <mulyan.t20@gmail.com>
2024-07-28 05:18:46 +0700

    fix: createAkunHandler

--- controller/akun.controller.js
+++ controller/akun.controller.js
@@ -1,13 +1,25 @@
 const db = require('../lib/db')
+const bcrypt = require('bcrypt')
 
 const createAkunHandler = async (req, res, next) => {
   try {
     const { nama, email, password, role } = req.body
+
+    const user = await db.akun.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (user) throw new Error('akun sudah ada')
+
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password || 'password', salt)
+
     await db.akun.create({
       data: {
         nama,
         email,
-        password,
+        password: hashPassword,
         role,
       },
     })
@@ -18,19 +30,20 @@ const createAkunHandler = async (req, res, next) => {
 }
 const updateAkunHandler = async (req, res, next) => {
   try {
-    const { nama, email, password, role } = req.body
     const { id } = req.params
+    const data = { ...req.body }
+    console.log(data)
+
+    if (data.password == '') delete data.password
+    console.log(data)
+
     await db.akun.update({
-      data: {
-        nama,
-        email,
-        password,
-        role,
-      },
+      data,
       where: {
         id: Number(id),
       },
     })
+
     return res.status(201).json({ message: 'Akun berhasil diupdate' })
   } catch (error) {
     next(error)
