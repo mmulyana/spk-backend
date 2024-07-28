70a853d292018b4d8e85842cc789fb83607d38fc
mmulyana <mulyan.t20@gmail.com>
2024-07-28 09:46:52 +0700

    feat: spkHandler

--- controller/spk.controller.js
+++ controller/spk.controller.js
@@ -1,6 +1,34 @@
-const hitungHandler = async (req, res, next) => {
+const db = require('../lib/db')
+
+const spkHandler = async (req, res, next) => {
   try {
+    const { id, kriteria } = req.body
+    const transformedArray = Object.entries(kriteria).map(([key, value]) => ({
+      id: Number(key),
+      value: value,
+    }))
+
+    await db.perhitungan.createMany({
+      data: transformedArray.map((d) => ({
+        idKriteria: d.id,
+        nilai: d.value,
+        idPegawai: id,
+      })),
+    })
+
+    await db.pegawai.update({
+      data: {
+        status: 'SUDAH',
+      },
+      where: {
+        id,
+      },
+    })
+
+    return res.status(201).json({ message: 'Pegawai berhasil dihitung' })
   } catch (error) {
     next(error)
   }
 }
+
+module.exports = { spkHandler }
