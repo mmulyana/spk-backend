56ae4f4edcda0eff1c69763b886a144aa59e6422
mmulyana <mulyan.t20@gmail.com>
2024-07-28 09:47:29 +0700

    feat: apply spkHandler

--- routes/spk.js
+++ routes/spk.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
+const { spkHandler } = require('../controller/spk.controller')
 
 const router = Router()
 
-router.post('/spk')
+router.post('/spk', spkHandler)
 
 module.exports = router
