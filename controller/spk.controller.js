5cb3f266c1519e44f3b710fc532ee975fe61ab04
mmulyana <mulyan.t20@gmail.com>
2024-08-01 00:29:33 +0700

    feat: apply spk helper in spk controller

--- controller/spk.controller.js
+++ controller/spk.controller.js
@@ -1,23 +1,43 @@
-const { calculateSPK } = require('../helper/spk-helper')
+const { calculateSPK, reCalculateSPK } = require('../helper/spk-helper')
 const db = require('../lib/db')
+const { hasData } = require('../utils/has-data')
 
 const spkHandler = async (req, res, next) => {
   try {
-    const { id, kriteria } = req.body
-    const transformedArray = Object.entries(kriteria).map(([key, value]) => ({
-      id: Number(key),
-      value: value,
-    }))
+    const { id, values } = req.body
 
     await db.perhitungan.createMany({
-      data: transformedArray.map((d) => ({
+      data: values.map((d) => ({
         idKriteria: d.id,
-        nilai: d.value,
+        nilai: d.nilai,
         idPegawai: id,
       })),
     })
 
     await calculateSPK(id)
+
+    const existingData = await Promise.all(
+      values.map(async (k) => {
+        return await db.perhitungan.findMany({
+          where: {
+            AND: [
+              {
+                idKriteria: k.id,
+              },
+              {
+                nilai: k.tipe == 'Benefit' ? { lt: k.nilai } : { gt: k.nilai },
+              },
+            ],
+          },
+          take: 1,
+        })
+      })
+    )
+
+    const isCalculateChanged = hasData(existingData)
+    if (isCalculateChanged) {
+      reCalculateSPK(id)
+    }
 
     await db.pegawai.update({
       data: {
